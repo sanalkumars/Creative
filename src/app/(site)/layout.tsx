8ac6dfb0b977aec6998459a7fee53ecfad2a4613
sanalkumars <sanalskumar126@gmail.com>
2024-02-29 22:16:38 +0530

    added middleware for authentication and completed header

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,8 +1,12 @@
+import Header from '@/components/landing-page/header'
 import React from 'react'
 
 const HomeLayout = ({ children } : { children:React.ReactNode } ) => {
   return (
-    <main>{children}</main>
+    <main>
+      <Header />
+      {children}
+      </main>
   )
 }
 
--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -80,7 +80,7 @@ const Header = () => {
       >
         <Image
           src={Logo}
-          alt="Cypress Logo"
+          alt="Creative Logo"
           width={25}
           height={25}
         />
@@ -89,7 +89,7 @@ const Header = () => {
           dark:text-white
         "
         >
-          cypress.
+          Creative.
         </span>
       </Link>
       <NavigationMenu className="hidden md:block">
